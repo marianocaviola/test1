57b4d930ec00d799f3e1b3dea71f7a8e00bea211
marianocaviola <caviolamariano@gmail.com>
2026-04-01 13:43:42 -0300

    Se plateó la estructura del codigo

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+// Prototipos exigidos por la consigna
+void primero(int a, int b);
+void segundo(int a, int b);
+void iguales();
+
+int main() {
+    int n1, n2;
+
+    // Dev-1
+
+    // Dev-2
+    
+    return 0;
+}
+
+// Dev-3
