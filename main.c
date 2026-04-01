599be10e1d2e12d1a563e1e0333efe304622d6a2
marianocaviola <caviolamariano@gmail.com>
2026-04-01 13:52:34 -0300

    Modifica Dev-3

--- main.c
+++ main.c
@@ -16,3 +16,14 @@ int main() {
 }
 
 // Dev-3
+void primero(int a, int b){
+    printf("El primer numero (%d) es el mayor\n", a);
+}
+
+void segundo(int a, int b){
+    printf("El segundo numero (%d) es el mayor\n", a);
+}
+
+void iguales(){
+    printf("Son iguales\n");
+}
